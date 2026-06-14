ba2d30835ef82ee6c22d23e699a292e46ce8bd14
isaachan <isaachanstar@gmail.com>
2026-06-14 20:03:10 +0800

    package

---
+++ electron/main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+    title: 'S58 Engine Trainer',
+    autoHideMenuBar: true,
+  })
+
+  win.loadFile(path.join(__dirname, '../dist/index.html'))
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  app.quit()
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
